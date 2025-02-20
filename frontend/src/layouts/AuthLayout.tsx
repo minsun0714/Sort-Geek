1daaa2c4c88706379fcc311702f68d0ba7695db9
김강민 <woooooooow22@gmail.com>
2025-02-20 23:51:12 +0900

    로그인 페이지 ui 생성

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div>
+    <div className="py-20 flex flex-col items-center justify-center h-2/3">
       <Outlet />
     </div>
   );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,52 @@
+import { useForm } from "react-hook-form";
+import Button from "../components/auth/Button";
+
+type LoginFormData = {
+  id: string;
+  password: string;
+};
+
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>();
+  const onSubmit = (data: LoginFormData) => console.log(data);
   return (
-    <div>
-      <h1>로그인</h1>
+    <div className="flex flex-col items-center justify-center w-1/4 h-2/3">
+      <h1 className="text-left text-3xl font-semibold w-full">로그인</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center justify-center h-2/3 w-full my-6"
+      >
+        <label className="py-2 w-full">아이디</label>
+        <input
+          {...register("id", { required: true })}
+          className="h-10 p-2 w-full border-2 border-blue-900 outline-none"
+        />
+        {errors.id ? (
+          <p className="text-red-600 text-left w-full">아이디를 입력해주세요</p>
+        ) : (
+          <div className="h-6" />
+        )}
+        <label className="py-2 w-full">비밀번호</label>
+        <input
+          {...register("password", { required: true })}
+          className="h-10 p-2 w-full border-2 border-blue-900 outline-none"
+        />
+        {errors.password ? (
+          <p className="text-red-600 text-left w-full">
+            비밀번호를 입력해주세요
+          </p>
+        ) : (
+          <div className="h-6" />
+        )}
+
+        <div className="w-full py-4">
+          <Button text="로그인" />
+        </div>
+      </form>
     </div>
   );
 }
