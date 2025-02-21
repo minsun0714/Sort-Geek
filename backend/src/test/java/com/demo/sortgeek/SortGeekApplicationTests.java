849aabbc72ea8dcde9696fcaec577cee327f4b51
이민선 <woooooooow22@gmail.com>
2025-02-21 19:53:21 +0900

    테스트 클래스에 어노테이션 사용하여 빌드 성공

--- backend/src/test/java/com/demo/sortgeek/SortGeekApplicationTests.java
+++ backend/src/test/java/com/demo/sortgeek/SortGeekApplicationTests.java
@@ -1,10 +1,16 @@
 package com.demo.sortgeek;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SortGeekApplicationTests {
 
 	@Test
