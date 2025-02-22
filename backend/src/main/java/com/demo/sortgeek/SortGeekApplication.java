3ccc885e0944b02a63000bb30fc11177f850c1e2
이민선 <woooooooow22@gmail.com>
2025-02-22 15:20:38 +0900

    JPA 레포지토리 base package 설정

--- backend/src/main/java/com/demo/sortgeek/SortGeekApplication.java
+++ backend/src/main/java/com/demo/sortgeek/SortGeekApplication.java
@@ -2,8 +2,10 @@ package com.demo.sortgeek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.demo.sortgeek.repository")
 public class SortGeekApplication {
 
 	public static void main(String[] args) {
