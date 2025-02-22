36c4604373dacceed3e0b894100f47e79077aaa2
이민선 <woooooooow22@gmail.com>
2025-02-22 18:38:34 +0900

    배포환경에 아직 HTTPS 적용이 안되었기 때문에 임시로 false로 바꾸고 테스트. 현재 리프레시 토큰이 쿠키에 발급되지 않는 문제를 해결해야 함.

--- backend/src/main/java/com/demo/sortgeek/security/utils/CookieUtils.java
+++ backend/src/main/java/com/demo/sortgeek/security/utils/CookieUtils.java
@@ -7,7 +7,7 @@ public class CookieUtils {
     public static void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(true); // HTTPS에서만 전송되도록 설정
+        refreshTokenCookie.setSecure(false); // HTTPS에서만 전송되도록 설정
         refreshTokenCookie.setPath("/");
         refreshTokenCookie.setMaxAge(24 * 60 * 60); // 24시간
         response.addCookie(refreshTokenCookie);
